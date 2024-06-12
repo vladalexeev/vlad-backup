e4010b99a8b91f076bc69f8187eeb3994efe509b
Vlad Alexeev <vlad.alexeev@gmail.com>
2024-06-12 17:36:30 +0300

    Add backup for music and catch exception

--- backup.py
+++ backup.py
@@ -19,84 +19,70 @@ LENOVO_BACKUP = '\\\\LENOVO-PC\\Vlad\\Backup\\'
 
 start_time = datetime.now()
 
-print('Synchronize with OneDrive: impossible')
-foldersync.sync(SITES_FOLDER+'impossible',
-                ONE_DRIVE_BACKUP+'impossible')
-  
-print('--------')
-print('Synchronize with OneDrive: zkaluga.avhost.info')
-foldersync.sync(SITES_FOLDER+'zkaluga.avhost.info',
-                ONE_DRIVE_BACKUP+'zkaluga.avhost.info')
-  
-print('--------')
-print('Synchronize with Lenovo: impossible')
-foldersync2.sync(SITES_FOLDER+'impossible',
-                 LENOVO_BACKUP+'Sites\\impossible',
-                 BACKUP_DATA_FOLDER+'impossible.data')
-  
-print('--------')
-print('Synchronize with Lenovo: zkaluga.avhost.info')
-foldersync2.sync(SITES_FOLDER+'zkaluga.avhost.info',
-                 LENOVO_BACKUP+'Sites\\zkaluga.avhost.info',
-                 BACKUP_DATA_FOLDER+'zkaluga.avhost.info.data')
+try:
 
-print('--------')
-print('Synchronize with Lenovo: ART_SORTED')
-foldersync2.sync(FROM_INET_FOLDER+'Important\\ART_SORTED',
-                 LENOVO_BACKUP+'ART_SORTED',
-                 BACKUP_DATA_FOLDER+'ART_SORTED.data')
+    print('Synchronize with OneDrive: impossible')
+    foldersync.sync(SITES_FOLDER+'impossible',
+                    ONE_DRIVE_BACKUP+'impossible')
 
-print('--------')
-print('Synchronize with Lenovo: IMP_SORTED')
-foldersync2.sync(FROM_INET_FOLDER+'Important\\IMP_SORTED',
-                 LENOVO_BACKUP+'IMP_SORTED',
-                 BACKUP_DATA_FOLDER+'IMP_SORTED.data')
+    print('--------')
+    print('Synchronize with OneDrive: zkaluga.avhost.info')
+    foldersync.sync(SITES_FOLDER+'zkaluga.avhost.info',
+                    ONE_DRIVE_BACKUP+'zkaluga.avhost.info')
 
-print('--------')
-print('Synchronize with Lenovo: unprocessed_images')
-foldersync2.sync(FROM_INET_FOLDER+'Important\\unprocessed_images',
-                 LENOVO_BACKUP+'unprocessed_images',
-                 BACKUP_DATA_FOLDER+'unprocessed_images.data')
+    print('--------')
+    print('Synchronize with Lenovo: impossible')
+    foldersync2.sync(SITES_FOLDER+'impossible',
+                     LENOVO_BACKUP+'Sites\\impossible',
+                     BACKUP_DATA_FOLDER+'impossible.data')
 
-print('--------')
-print('Synchronize with Lenovo: impossible-video')
-foldersync2.sync(FROM_INET_FOLDER+'impossible-video',
-                 LENOVO_BACKUP+'impossible-video',
-                 BACKUP_DATA_FOLDER+'impossible_video.data')
+    print('--------')
+    print('Synchronize with Lenovo: zkaluga.avhost.info')
+    foldersync2.sync(SITES_FOLDER+'zkaluga.avhost.info',
+                     LENOVO_BACKUP+'Sites\\zkaluga.avhost.info',
+                     BACKUP_DATA_FOLDER+'zkaluga.avhost.info.data')
 
-print('--------')
-print('Synchronize with Lenovo: Backup-Data')
-foldersync.sync(BACKUP_DATA_FOLDER, 
-                LENOVO_BACKUP+'Backup-data')
+    print('--------')
+    print('Synchronize with Lenovo: ART_SORTED')
+    foldersync2.sync(FROM_INET_FOLDER+'Important\\ART_SORTED',
+                     LENOVO_BACKUP+'ART_SORTED',
+                     BACKUP_DATA_FOLDER+'ART_SORTED.data')
 
+    print('--------')
+    print('Synchronize with Lenovo: IMP_SORTED')
+    foldersync2.sync(FROM_INET_FOLDER+'Important\\IMP_SORTED',
+                     LENOVO_BACKUP+'IMP_SORTED',
+                     BACKUP_DATA_FOLDER+'IMP_SORTED.data')
 
+    print('--------')
+    print('Synchronize with Lenovo: unprocessed_images')
+    foldersync2.sync(FROM_INET_FOLDER+'Important\\unprocessed_images',
+                     LENOVO_BACKUP+'unprocessed_images',
+                     BACKUP_DATA_FOLDER+'unprocessed_images.data')
 
-# print('Archive: impossible')
-# file_util.roll_file_stack(SITES_FOLDER+'impossible{}.zip', 5)
-# shutil.make_archive(
-#     SITES_FOLDER+'impossible', 
-#     'zip', SITES_FOLDER,
-#     'impossible')
+    print('--------')
+    print('Synchronize with Lenovo: impossible-video')
+    foldersync2.sync(FROM_INET_FOLDER+'impossible-video',
+                     LENOVO_BACKUP+'impossible-video',
+                     BACKUP_DATA_FOLDER+'impossible_video.data')
 
-# print('Archive: unprocessed images')
-# file_util.roll_file_stack(FROM_INET_FOLDER+'Important\\unprocessed_images{}.zip', 2)
-# shutil.make_archive(
-#     FROM_INET_FOLDER+'Important\\unprocessed_images', 
-#     'zip', FROM_INET_FOLDER+'Important\\',
-#     'unprocessed_images')
+    print('--------')
+    print('Synchronize with Lenovo: Music-car')
+    foldersync.sync('C:\\VladWork\\Music-car',
+                    LENOVO_BACKUP+'Music-car')
 
+    print('--------')
+    print('Synchronize with Lenovo: Backup-Data')
+    foldersync.sync(BACKUP_DATA_FOLDER,
+                    LENOVO_BACKUP+'Backup-data')
 
-# print('Copy to Lenovo: impossible.zip')
-# file_util.roll_file_stack(LENOVO_BACKUP+'impossible{}.zip', 5)
-# shutil.copy2(SITES_FOLDER+'impossible.zip', LENOVO_BACKUP+'impossible.zip')
+    print('')
+    print('Finished! {}'.format(datetime.now() - start_time))
+
+    input("Press Enter to continue...")
+except Exception as e:
+    import traceback
+    traceback.print_exc()
+    input("Press Enter to continue...")
 
 
-# print('Copy to Lenovo: unprocessed_images.zip')
-# file_util.roll_file_stack(LENOVO_BACKUP+'unprocessed_images{}.zip', 2)
-# shutil.copy2(FROM_INET_FOLDER+'Important\\unprocessed_images.zip', LENOVO_BACKUP+'unprocessed_images.zip')
-
-
-print('')
-print('Finished! {}'.format(datetime.now() - start_time))
-
-input("Press Enter to continue...")
